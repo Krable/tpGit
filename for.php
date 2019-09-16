3219036564ed683a12462477a9013f2925d4299a
lemoine <lemoinelucas1@gmail.com>
2019-09-16 12:29:52 +0200

    MoreCommit2
    MoreCommit3
    
    MoreCommit5

--- for.php
+++ for.php
@@ -5,6 +5,27 @@ $eleve = ['nom'=>'lemoine', 'prenom'=>'lucas', 'age'=>'19'];
 $eleve = ['nom'=>'lanovaz', 'prenom'=>'leo', 'age'=>'21'];
 $eleve = ['nom'=>'lemoine', 'prenom'=>'lucas', 'age'=>'19'];
 $eleve = ['nom'=>'lanovaz', 'prenom'=>'leo', 'age'=>'21'];
+$eleve = ['nom'=>'lemoine', 'prenom'=>'lucas', 'age'=>'19'];
+$eleve = ['nom'=>'lanovaz', 'prenom'=>'leo', 'age'=>'21'];
+$eleve = ['nom'=>'lemoine', 'prenom'=>'lucas', 'age'=>'19'];
+$eleve = ['nom'=>'lanovaz', 'prenom'=>'leo', 'age'=>'21'];
+$eleve = ['nom'=>'lemoine', 'prenom'=>'lucas', 'age'=>'19'];
+$eleve = ['nom'=>'lanovaz', 'prenom'=>'leo', 'age'=>'21'];
+$eleve = ['nom'=>'lemoine', 'prenom'=>'lucas', 'age'=>'19'];
+$eleve = ['nom'=>'lanovaz', 'prenom'=>'leo', 'age'=>'21'];
+$eleve = ['nom'=>'lemoine', 'prenom'=>'lucas', 'age'=>'19'];
+$eleve = ['nom'=>'lanovaz', 'prenom'=>'leo', 'age'=>'21'];
+$eleve = ['nom'=>'lemoine', 'prenom'=>'lucas', 'age'=>'19'];
+$eleve = ['nom'=>'lanovaz', 'prenom'=>'leo', 'age'=>'21'];
+$eleve = ['nom'=>'lemoine', 'prenom'=>'lucas', 'age'=>'19'];
+$eleve = ['nom'=>'lanovaz', 'prenom'=>'leo', 'age'=>'21'];
+$eleve = ['nom'=>'lemoine', 'prenom'=>'lucas', 'age'=>'19'];
+$eleve = ['nom'=>'lanovaz', 'prenom'=>'leo', 'age'=>'21'];
+$eleve = ['nom'=>'lemoine', 'prenom'=>'lucas', 'age'=>'19'];
+$eleve = ['nom'=>'lanovaz', 'prenom'=>'leo', 'age'=>'21'];
+$eleve = ['nom'=>'lemoine', 'prenom'=>'lucas', 'age'=>'19'];
+$eleve = ['nom'=>'lanovaz', 'prenom'=>'leo', 'age'=>'21'];
+
 
 for(int $i = 0; $i < 10; $i++)
 {
@@ -13,6 +34,16 @@ for(int $i = 0; $i < 10; $i++)
 
 $i
 while($i < 145112 && $i != 2451116514)
+{
+	echo "Numéro : " . $i;
+}
+
+foreach($tab as $value)
+{
+	echo "Numéro : " . $i;
+}
+
+foreach($tab as $value)
 {
 	echo "Numéro : " . $i;
 }
